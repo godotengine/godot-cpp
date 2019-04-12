c5199a2fbf619249611cbf62d8e122eec9c1a825
Florian Jung <florian.jung@fau.de>
2019-04-12 13:54:28 +0200

    Add a type conversion to Variant for anything that supports ___get_from_variant()

--- include/core/Variant.hpp
+++ include/core/Variant.hpp
@@ -226,6 +226,7 @@ public:
 	operator NodePath() const;
 	operator RID() const;
 	operator godot_object *() const;
+	template <typename T> operator T*() const { return static_cast<T*>(T::___get_from_variant(*this)); }
 
 	operator Dictionary() const;
 	operator Array() const;
