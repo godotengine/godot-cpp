4912229698807797d1f9c090fb80e5571800e426
Philius342 <philipp.wundrack@live.de>
2019-11-22 22:46:19 +0100

    Reset reference counter to fix memory leak

--- include/core/Godot.hpp
+++ include/core/Godot.hpp
@@ -43,10 +43,14 @@ public:                                                                         
 		script->set_library(godot::get_wrapper<godot::GDNativeLibrary>((godot_object *)godot::gdnlib));                                             \
 		script->set_class_name(#Name);                                                                                                              \
 		Name *instance = godot::as<Name>(script->new_());                                                                                           \
+		godot::Reference *ref = (godot::Reference *)instance;                                                                                       \
+		if (ref) {                                                                                                                                  \
+			ref->reset_ref();                                                                                                                       \
+		}                                                                                                                                           \
 		return instance;                                                                                                                            \
 	}                                                                                                                                               \
-	inline static size_t ___get_id() { return typeid(Name).hash_code(); }                                                                          \
-	inline static size_t ___get_base_id() { return typeid(Base).hash_code(); }                                                                     \
+	inline static size_t ___get_id() { return typeid(Name).hash_code(); }                                                                           \
+	inline static size_t ___get_base_id() { return typeid(Base).hash_code(); }                                                                      \
 	inline static const char *___get_base_type_name() { return Base::___get_class_name(); }                                                         \
 	inline static Object *___get_from_variant(godot::Variant a) { return (godot::Object *)godot::as<Name>(godot::Object::___get_from_variant(a)); } \
                                                                                                                                                     \
