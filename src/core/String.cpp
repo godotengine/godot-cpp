7a1890345b2658b422823bce889f88cc7dbb76d5
DhruvMaroo <dhruvmaru007@gmail.com>
2021-06-01 23:16:09 +0530

    edited according to clang-format

--- src/core/String.cpp
+++ src/core/String.cpp
@@ -73,7 +73,7 @@ String::String(const String &other) {
 	godot::api->godot_string_new_copy(&_godot_string, &other._godot_string);
 }
 
-String::String(String&& other) {
+String::String(String &&other) {
 	godot::api->godot_string_new_copy(&_godot_string, &other._godot_string);
 }
 
@@ -98,7 +98,7 @@ void String::operator=(const String &s) {
 	godot::api->godot_string_new_copy(&_godot_string, &s._godot_string);
 }
 
-void String::operator=(String&& s) {
+void String::operator=(String &&s) {
 	godot::api->godot_string_destroy(&_godot_string);
 	godot::api->godot_string_new_copy(&_godot_string, &s._godot_string);
 }
