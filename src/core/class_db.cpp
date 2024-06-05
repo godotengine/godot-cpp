c6d30a8d4457c647748ae4234a710519dfa7143e
Daylily-Zeleen <daylily-zeleen@foxmail.com>
2024-06-05 16:19:37 +0800

    Fix compile warnings.

--- src/core/class_db.cpp
+++ src/core/class_db.cpp
@@ -353,7 +353,7 @@ void ClassDB::add_virtual_method(const StringName &p_class, const MethodInfo &p_
 	if (mi.argument_count > 0) {
 		mi.arguments = (GDExtensionPropertyInfo *)memalloc(sizeof(GDExtensionPropertyInfo) * mi.argument_count);
 		mi.arguments_metadata = (GDExtensionClassMethodArgumentMetadata *)memalloc(sizeof(GDExtensionClassMethodArgumentMetadata) * mi.argument_count);
-		for (int i = 0; i < mi.argument_count; i++) {
+		for (uint32_t i = 0; i < mi.argument_count; i++) {
 			mi.arguments[i] = p_method.arguments[i]._to_gdextension();
 			mi.arguments_metadata[i] = p_method.arguments_metadata[i];
 		}
@@ -365,7 +365,7 @@ void ClassDB::add_virtual_method(const StringName &p_class, const MethodInfo &p_
 	if (p_arg_names.size() != mi.argument_count) {
 		WARN_PRINT("Mismatch argument name count for virtual method: " + String(p_class) + "::" + p_method.name);
 	} else {
-		for (int i = 0; i < p_arg_names.size(); i++) {
+		for (size_t i = 0; i < p_arg_names.size(); i++) {
 			mi.arguments[i].name = (GDExtensionStringNamePtr)&p_arg_names[i];
 		}
 	}
@@ -429,7 +429,7 @@ void ClassDB::deinitialize(GDExtensionInitializationLevel p_level) {
 		{
 			std::lock_guard<std::mutex> lock(engine_singletons_mutex);
 			singleton_objects.reserve(engine_singletons.size());
-			for (const std::pair<StringName, Object *> &pair : engine_singletons) {
+			for (const std::pair<const StringName, Object *> &pair : engine_singletons) {
 				singleton_objects.push_back(pair.second);
 			}
 		}
