762db4e4d6eb4ed496a5f5c05442e49b2750d96d
Aaron Franke <arnfranke@yahoo.com>
2024-08-21 18:31:12 -0700

    Fix missing MAKE_TYPED_ARRAY_INFO for Packed*Arrays
    (cherry picked from commit 10c3d1bc5f7f68bb4e260f32b9a8b529d23873ba)

--- test/src/example.h
+++ test/src/example.h
@@ -134,6 +134,7 @@ public:
 	String test_str_utility() const;
 	bool test_string_is_forty_two(const String &p_str) const;
 	String test_string_resize(String p_original) const;
+	TypedArray<PackedInt32Array> test_typed_array_of_packed() const;
 	int test_vector_ops() const;
 	int test_vector_init_list() const;
 
