cb41b472f458bc874aa8b715ff889a070a91c4b1
Zhehang Ding <zhehangd@gmail.com>
2024-10-07 20:51:51 +0800

    Use namespace in defs.hpp
    A global alias of godot::real_t is defined for backward compatibility
    
    (cherry picked from commit 450c3d65cd182b3e33a1deeae250143e699f7972)

--- include/godot_cpp/core/defs.hpp
+++ include/godot_cpp/core/defs.hpp
@@ -35,6 +35,8 @@
 #include <cstdint>
 #include <cstring>
 
+namespace godot {
+
 #if !defined(GDE_EXPORT)
 #if defined(_WIN32)
 #define GDE_EXPORT __declspec(dllexport)
@@ -126,5 +128,11 @@ struct BuildIndexSequence : BuildIndexSequence<N - 1, N - 1, Is...> {};
 
 template <size_t... Is>
 struct BuildIndexSequence<0, Is...> : IndexSequence<Is...> {};
+
+} //namespace godot
+
+// To maintain compatibility an alias is defined outside the namespace.
+// Consider it deprecated.
+using real_t = godot::real_t;
 
 #endif // GODOT_DEFS_HPP
