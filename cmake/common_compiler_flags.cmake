af8f04cd9efa2b87eaf7d3397322d293aff20de7
Samuel Nicholas <nicholas.samuel@gmail.com>
2024-12-07 10:26:27 +1030

    Privatise and Tidy
    There are tiny issues that don't effect the build that can be cleaned.
    And flags that can be made private to not effect consumers.
    
    Remove RPATH_USE_ORIGIN from static libs
    add INTERFACE_POSITION_INDEPENDENT_CODE ON
    reset GODOT_ARCH property to SYSTEM_ARCH for macos because universal
    add MACOSX_RPATH to test target
    remove redundant properties from test target
    remove redundant and PUBLIC cxx_std_17 compile feature
    Change compiler options to PRIVATE, tested on godot-cpp-test, and orchestrator

--- cmake/common_compiler_flags.cmake
+++ cmake/common_compiler_flags.cmake
@@ -19,11 +19,11 @@ set( IS_GNU "$<CXX_COMPILER_ID:GNU>" )
 set( IS_MSVC "$<CXX_COMPILER_ID:MSVC>" )
 set( NOT_MSVC "$<NOT:$<CXX_COMPILER_ID:MSVC>>" )
 
-set( GNU_LT_V8 "$<VERSION_LESS:$<CXX_COMPILER_VERSION>,8>" )
-set( GNU_GE_V9 "$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,9>" )
-set( GNU_GT_V11 "$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,11>" )
-set( GNU_LT_V11 "$<VERSION_LESS:$<CXX_COMPILER_VERSION>,11>" )
-set( GNU_GE_V12 "$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,12>" )
+set( GNU_LT_V8 "$<${IS_GNU}:$<VERSION_LESS:$<CXX_COMPILER_VERSION>,8>>" )
+set( GNU_GE_V9 "$<${IS_GNU}:$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,9>>" )
+set( GNU_GT_V11 "$<${IS_GNU}:$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,11>>" )
+set( GNU_LT_V11 "$<${IS_GNU}:$<VERSION_LESS:$<CXX_COMPILER_VERSION>,11>>" )
+set( GNU_GE_V12 "$<${IS_GNU}:$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,12>>" )
 
 #[[ Check for clang-cl with MSVC frontend
 The compiler is tested and set when the project command is called.
@@ -46,19 +46,12 @@ endfunction(  )
 
 function( common_compiler_flags TARGET_NAME )
 
-    target_compile_features(${TARGET_NAME}
-            PUBLIC
-            cxx_std_17
-    )
-
     # These compiler options reflect what is in godot/SConstruct.
     target_compile_options( ${TARGET_NAME}
-        PUBLIC
+        PRIVATE
             # Disable exception handling. Godot doesn't use exceptions anywhere, and this
             # saves around 20% of binary size and very significant build time.
-            $<${DISABLE_EXCEPTIONS}:
-                $<${NOT_MSVC}:-fno-exceptions>
-            >
+            $<${DISABLE_EXCEPTIONS}:$<${NOT_MSVC}: -fno-exceptions >>
 
             # Enabling Debug Symbols
             $<${DEBUG_SYMBOLS}:
@@ -74,15 +67,15 @@ function( common_compiler_flags TARGET_NAME )
                 $<${NOT_MSVC}:-fno-omit-frame-pointer -O0>
             >
 
-            $<${HOT_RELOAD}:
-                $<${IS_GNU}:-fno-gnu-unique>
-            >
+            $<${HOT_RELOAD}:$<${IS_GNU}: -fno-gnu-unique>>
 
         # MSVC only
         $<${IS_MSVC}:
             # /MP isn't valid for clang-cl with msvc frontend
             $<$<CXX_COMPILER_ID:MSVC>:/MP${PROC_N}>
             /W4
+
+            /W4      # Warning level 4 (informational) warnings that aren't off by default.
 
             # Disable warnings which we don't plan to fix.
             /wd4100  # C4100 (unreferenced formal parameter): Doesn't play nice with polymorphism.
@@ -95,8 +88,6 @@ function( common_compiler_flags TARGET_NAME )
             /wd4514  # C4514 (unreferenced inline function has been removed)
             /wd4714  # C4714 (function marked as __forceinline not inlined)
             /wd4820  # C4820 (padding added after construct)
-
-            /utf-8
         >
 
         # Clang and GNU common options
@@ -124,21 +115,22 @@ function( common_compiler_flags TARGET_NAME )
             -Wplacement-new=1
             -Wshadow-local
             -Wstringop-overflow=4
-
-            # Bogus warning fixed in 8+.
-            $<${GNU_LT_V8}:-Wno-strict-overflow>
-
-            $<${GNU_GE_V9}:-Wattribute-alias=2>
-
-            # Broke on MethodBind templates before GCC 11.
-            $<${GNU_GT_V11}:-Wlogical-op>
-
-            # Regression in GCC 9/10, spams so much in our variadic templates that we need to outright disable it.
-            $<${GNU_LT_V11}:-Wno-type-limits>
-
-            # False positives in our error macros, see GH-58747.
-            $<${GNU_GE_V12}:-Wno-return-type>
         >
+
+        # Bogus warning fixed in 8+.
+        $<${GNU_LT_V8}: -Wno-strict-overflow >
+
+        $<${GNU_GE_V9}: -Wattribute-alias=2 >
+
+        # Broke on MethodBind templates before GCC 11.
+        $<${GNU_GT_V11}: -Wlogical-op >
+
+        # Regression in GCC 9/10, spams so much in our variadic templates that we need to outright disable it.
+        $<${GNU_LT_V11}: -Wno-type-limits >
+
+        # False positives in our error macros, see GH-58747.
+        $<${GNU_GE_V12}: -Wno-return-type >
+
     )
 
     target_compile_definitions(${TARGET_NAME}
@@ -146,29 +138,27 @@ function( common_compiler_flags TARGET_NAME )
             GDEXTENSION
 
             # features
-            $<${DEBUG_FEATURES}:DEBUG_ENABLED DEBUG_METHODS_ENABLED>
+            $<${DEBUG_FEATURES}: DEBUG_ENABLED DEBUG_METHODS_ENABLED >
 
             $<${IS_DEV_BUILD}:DEV_ENABLED>
 
             $<${HOT_RELOAD}:HOT_RELOAD_ENABLED>
 
-            $<$<STREQUAL:${GODOT_PRECISION},double>:REAL_T_IS_DOUBLE>
+            $<$<STREQUAL:${GODOT_PRECISION},double>: REAL_T_IS_DOUBLE >
 
-            $<${IS_MSVC}:$<${DISABLE_EXCEPTIONS}:_HAS_EXCEPTIONS=0>>
+            $<${IS_MSVC}:$<${DISABLE_EXCEPTIONS}: _HAS_EXCEPTIONS=0 >>
     )
 
     target_link_options( ${TARGET_NAME}
-        PUBLIC
-            $<${IS_MSVC}:
-                /WX             # treat link warnings as errors.
-                /MANIFEST:NO    # We dont need a manifest
-            >
+        PRIVATE
+            $<${IS_MSVC}: /WX /MANIFEST:NO >
+            # /WX             # treat link warnings as errors.
+            # /MANIFEST:NO    # We dont need a manifest
 
-            $<${DEBUG_SYMBOLS}:$<${IS_MSVC}:/DEBUG:FULL>>
             $<$<NOT:${DEBUG_SYMBOLS}>:
-                $<${IS_GNU}:-s>
-                $<${IS_CLANG}:-s>
-                $<${IS_APPLECLANG}:-Wl,-S -Wl,-x -Wl,-dead_strip>
+                $<${IS_GNU}: -s >
+                $<${IS_CLANG}: -s >
+                $<${IS_APPLECLANG}: -Wl,-S -Wl,-x -Wl,-dead_strip >
             >
     )
 
