a7840644e3c904a181e9b9ba257217f2fc73026c
Samuel Nicholas <nicholas.samuel@gmail.com>
2024-12-29 12:44:46 +1030

    Update Comments - Add TODO items for missing options - Add missing toolchain managed options commented out - Homogenise Headings and block comments

--- cmake/emsdkHack.cmake
+++ cmake/emsdkHack.cmake
@@ -31,7 +31,7 @@ if( EMSCRIPTEN )
     set(CMAKE_STRIP FALSE)  # used by default in pybind11 on .so modules
 
     # The Emscripten toolchain sets the default value for EMSCRIPTEN_SYSTEM_PROCESSOR to x86
-    # and CMAKE_SYSTEM_PROCESSOR to this value. I don't want that.
+    # and copies that to CMAKE_SYSTEM_PROCESSOR. We dont want that.
     set(CMAKE_SYSTEM_PROCESSOR "wasm32" )
     # the above prevents the need for logic like:
     #if( ${CMAKE_SYSTEM_NAME} STREQUAL Emscripten )
