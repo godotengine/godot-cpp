ab95ecebd15efbb095f9d3872b0be5ff77f6ec5b
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2024-12-30 17:10:36 +0100

    [CI] Re-add generated files consistency check

--- misc/scripts/check_get_file_list.py
+++ misc/scripts/check_get_file_list.py
@@ -13,19 +13,32 @@ bits = "64"
 precision = "single"
 output_dir = "self_test"
 
-generate_bindings(api_filepath, use_template_get_node=False, bits=bits, precision=precision, output_dir=output_dir)
-flist = get_file_list(api_filepath, output_dir, headers=True, sources=True)
 
-p = Path(output_dir) / "gen"
-allfiles = [str(f.as_posix()) for f in p.glob("**/*.*")]
-missing = list(filter((lambda f: f not in flist), allfiles))
-extras = list(filter((lambda f: f not in allfiles), flist))
-if len(missing) > 0 or len(extras) > 0:
-    print("Error!")
-    for f in missing:
-        print("MISSING: " + str(f))
-    for f in extras:
-        print("EXTRA: " + str(f))
-    sys.exit(1)
-else:
-    print("OK!")
+def test(profile_filepath=""):
+    generate_bindings(
+        api_filepath,
+        use_template_get_node=False,
+        bits=bits,
+        precision=precision,
+        output_dir=output_dir,
+        profile_filepath=profile_filepath,
+    )
+    flist = get_file_list(api_filepath, output_dir, headers=True, sources=True, profile_filepath=profile_filepath)
+
+    p = Path(output_dir) / "gen"
+    allfiles = [str(f.as_posix()) for f in p.glob("**/*.*")]
+    missing = list(filter((lambda f: f not in flist), allfiles))
+    extras = list(filter((lambda f: f not in allfiles), flist))
+    if len(missing) > 0 or len(extras) > 0:
+        print("Error!")
+        for f in missing:
+            print("MISSING: " + str(f))
+        for f in extras:
+            print("EXTRA: " + str(f))
+        sys.exit(1)
+    else:
+        print("OK!")
+
+
+test("test/build_profile.json")
+test()
