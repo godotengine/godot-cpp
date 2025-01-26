a8476dbbaebfed14a46a5db7d46cef44bacdc396
Ben Lubar <ben.lubar@gmail.com>
2025-01-25 18:40:33 -0600

    Make Math::fast_ftoi thread-safe

--- include/godot_cpp/core/math.hpp
+++ include/godot_cpp/core/math.hpp
@@ -766,7 +766,7 @@ inline double pingpong(double value, double length) {
 
 // This function should be as fast as possible and rounding mode should not matter.
 inline int fast_ftoi(float a) {
-	static int b;
+	int b;
 
 #if (defined(_WIN32_WINNT) && _WIN32_WINNT >= 0x0603) || (defined(WINAPI_FAMILY) && WINAPI_FAMILY == WINAPI_FAMILY_PHONE_APP) // windows 8 phone?
 	b = (int)((a > 0.0) ? (a + 0.5) : (a - 0.5));
