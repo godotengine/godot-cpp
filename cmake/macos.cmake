4b5d800596f8ea74413dfd38531a2938d65b512d
Samuel Nicholas <nicholas.samuel@gmail.com>
2025-03-02 12:04:30 +1030

    Updating commenting to be consistent
    (cherry picked from commit 671e309cfa2c06ab60cb528aa7937bf6d65c09c2)

--- cmake/macos.cmake
+++ cmake/macos.cmake
@@ -5,9 +5,12 @@ MacOS
 This file contains functions for options and configuration for targeting the
 MacOS platform
 
-# To build universal binaries, ie targeting both x86_64 and arm64, use
-# the CMAKE_OSX_ARCHITECTURES variable prior to any project calls.
-# https://cmake.org/cmake/help/latest/variable/CMAKE_OSX_ARCHITECTURES.html
+Universal Builds
+----------------
+
+To build universal binaries, ie targeting both x86_64 and arm64, use
+the CMAKE_OSX_ARCHITECTURES variable prior to any project calls.
+https://cmake.org/cmake/help/latest/variable/CMAKE_OSX_ARCHITECTURES.html
 
 ]=======================================================================]
 
@@ -24,9 +27,21 @@ if(APPLE)
     )
 endif(APPLE)
 
+#[=============================[ MacOS Options ]=============================]
 function(macos_options)
+    #[[ Options from SCons
+    TODO macos_deployment_target: macOS deployment target
+        Default: 'default'
+
+    TODO macos_sdk_path: macOS SDK path
+        Default: ''
+
+    TODO osxcross_sdk: OSXCross SDK version
+        Default: if has_osxcross(): "darwin16" else None
+    ]]
 endfunction()
 
+#[===========================[ Target Generation ]===========================]
 function(macos_generate)
     target_compile_definitions(${TARGET_NAME} PUBLIC MACOS_ENABLED UNIX_ENABLED)
 
--- cmake/web.cmake
+++ cmake/web.cmake
@@ -10,17 +10,13 @@ Web platform
 # Emscripten requires this hack for use of the SHARED option
 set(CMAKE_PROJECT_godot-cpp_INCLUDE cmake/emsdkHack.cmake)
 
+#[==============================[ Web Options ]==============================]
 function(web_options)
-    # web options
 endfunction()
 
+#[===========================[ Target Generation ]===========================]
 function(web_generate)
-    target_compile_definitions(
-        ${TARGET_NAME}
-        PUBLIC #
-            WEB_ENABLED
-            UNIX_ENABLED
-    )
+    target_compile_definitions(${TARGET_NAME} PUBLIC WEB_ENABLED UNIX_ENABLED)
 
     target_compile_options(
         ${TARGET_NAME}
