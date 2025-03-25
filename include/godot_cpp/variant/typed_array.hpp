b4bce00b32c13917decbace51d8af12e0765694d
Tom <tom_full@hotmail.co.uk>
2025-03-25 21:49:18 +0000

    Updated typed array initalizer list constructors to be more like the godot versions.

--- include/godot_cpp/variant/typed_array.hpp
+++ include/godot_cpp/variant/typed_array.hpp
@@ -56,7 +56,7 @@ public:
 	_FORCE_INLINE_ TypedArray() {
 		set_typed(Variant::OBJECT, T::get_class_static(), Variant());
 	}
-	_FORCE_INLINE_ TypedArray(const std::initializer_list<T> &p_init) {
+	_FORCE_INLINE_ TypedArray(const std::initializer_list<Variant> &p_init) {
 		set_typed(Variant::OBJECT, T::get_class_static(), Variant());
 		for (auto &element : p_init) {
 			append(element);
@@ -88,7 +88,7 @@ public:
 		_FORCE_INLINE_ TypedArray() {                                                                            \
 			set_typed(m_variant_type, StringName(), Variant());                                                  \
 		}                                                                                                        \
-		_FORCE_INLINE_ TypedArray(const std::initializer_list<m_type> &p_init) {                                 \
+		_FORCE_INLINE_ TypedArray(const std::initializer_list<Variant> &p_init) {                                \
 			set_typed(m_variant_type, StringName(), Variant());                                                  \
 			for (auto &element : p_init) {                                                                       \
 				append(element);                                                                                 \
