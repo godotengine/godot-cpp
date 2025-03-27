2efb83adc2fc3d1d7725d50750bdff904373a84a
Tom <tom_full@hotmail.co.uk>
2025-03-27 23:15:49 +0000

    Updated typed dictionary initializer_list constructor to match godot

--- include/godot_cpp/variant/typed_dictionary.hpp
+++ include/godot_cpp/variant/typed_dictionary.hpp
@@ -122,7 +122,7 @@ public:
 		}                                                                                                                  \
 		_FORCE_INLINE_ TypedDictionary(std::initializer_list<KeyValue<m_type, T>> p_init) :                                \
 				Dictionary() {                                                                                             \
-			set_typed(m_variant_type, StringName(), Variant(), Variant::OBJECT, T::get_class_static(), Variant());         \
+			set_typed(m_variant_type, StringName(), Variant(), Variant::OBJECT, StringName(), Variant());                  \
 			for (const KeyValue<m_type, T> &E : p_init) {                                                                  \
 				operator[](E.key) = E.value;                                                                               \
 			}                                                                                                              \
