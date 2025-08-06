8646cd31b52a86769aad7c8f5b0f06b42f1f11f8
Pāvels Nadtočajevs <7645683+bruvzg@users.noreply.github.com>
2025-08-06 22:48:13 +0300

    Sync Android SDK and NDK versions with the engine.

--- cmake/android.cmake
+++ cmake/android.cmake
@@ -34,7 +34,7 @@ function(android_options)
     more information
 
     android_api_level : Target Android API level.
-        Default = 21
+        Default = 24
 
     ANDROID_HOME : Path to your Android SDK installation.
         Default = os.environ.get("ANDROID_HOME", os.environ.get("ANDROID_SDK_ROOT")
